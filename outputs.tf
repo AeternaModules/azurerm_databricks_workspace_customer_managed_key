aaafdec755a9f3fc23424a7019995bbd613f4d6c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:44:17 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "databricks_workspace_customer_managed_keys" {
+  description = "All databricks_workspace_customer_managed_key resources"
+  value       = azurerm_databricks_workspace_customer_managed_key.databricks_workspace_customer_managed_keys
+}
+
