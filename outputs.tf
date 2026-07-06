47e4523d1c8defdb541d633234b00c4e494a95e3
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "databricks_workspace_customer_managed_keys" {
   description = "All databricks_workspace_customer_managed_key resources"
   value       = azurerm_databricks_workspace_customer_managed_key.databricks_workspace_customer_managed_keys
 }
+output "databricks_workspace_customer_managed_keys_key_vault_key_id" {
+  description = "List of key_vault_key_id values across all databricks_workspace_customer_managed_keys"
+  value       = [for k, v in azurerm_databricks_workspace_customer_managed_key.databricks_workspace_customer_managed_keys : v.key_vault_key_id]
+}
+output "databricks_workspace_customer_managed_keys_workspace_id" {
+  description = "List of workspace_id values across all databricks_workspace_customer_managed_keys"
+  value       = [for k, v in azurerm_databricks_workspace_customer_managed_key.databricks_workspace_customer_managed_keys : v.workspace_id]
+}
 
